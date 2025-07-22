e9f6ce21246dadc026f02c638ddf2326df2ba7bb
Flixjod <144277345+Flixjod@users.noreply.github.com>
2025-07-23 01:25:07 +0530

    Update Acc_login.py

--- FLiX/Acc_login.py
+++ FLiX/Acc_login.py
@@ -170,6 +170,7 @@ async def login_acc(client: Client, message: Message):
         if not otp_code:
             return
         otp_code = re.sub(r'\D', '', otp_code)
+        otp_code = " ".join(otp_code)
 
         try:
             await temp_client.sign_in(phone_number, code.phone_code_hash, otp_code)
